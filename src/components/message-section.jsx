c07bd5c036d3f7eab611db416fb6d321a7b95522
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-01-25 12:33:22 +0500

    Quizly - message section

--- src/components/message-section.jsx
+++ src/components/message-section.jsx
@@ -1,18 +1,20 @@
 import MaxWidthWrapper from "./max-width-wrapper"
+import LargeGreenStar from '../assets/img/stars/large-green-star.svg';
 
 const MessageSection = () => {
   return (
     <div className="py-16">
       <MaxWidthWrapper>
-        <div className="grid grid-cols-2 w-full lg:w-[80%] justify-self-center border border-red-600 px-2" >
-          <div>
-            <p>A Message to Ambitious Entrepreneurs</p>
+        <div className="w-full lg:w-[90%] justify-self-center border border-red-600 px-2">
+          <img className="aspect-square" src={LargeGreenStar} alt="Large-Green-Star" />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-0" >
+            <p className="font-myFont text-4xl/14 md:text-5xl/20 font-semibold text-myBlack ">A Message to Ambitious Entrepreneurs</p>
+            <div className="font-myFont text-myBlack text-xl/9">
+              <p className="font-semibold text-myGrayText">Growing a business can be overwhelming, with constant challenges like managing budgets, clients, and employees while staying on top of marketing strategies.</p>
+              <p className="font-bold text-myGrayText">At Lenob, we simplify growth by helping you focus on what matters most—building a business you love.</p>
+              <p className="font-bold text-red-500">Let us turn your big dreams into reality. Ready to take the next step? Let's build something great together.</p>
+            </div>            
           </div>
-          <div>
-            Growing a business can be overwhelming, with constant challenges like managing budgets, clients, and employees while staying on top of marketing strategies. 
-            At Lenob, we simplify growth by helping you focus on what matters most—building a business you love.&nbsp;
-            <span>Let us turn your big dreams into reality. Ready to take the next step? Let's build something great together.</span>
-          </div>            
         </div>
       </MaxWidthWrapper>
     </div>
