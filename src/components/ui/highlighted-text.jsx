1bc681cc3e2b326714afab517101380019b155c3
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-02-20 08:53:25 +0500

    Edit Highlighted Text

--- src/components/ui/highlighted-text.jsx
+++ src/components/ui/highlighted-text.jsx
@@ -1,7 +1,7 @@
 const HighlightedText = ({text}) => {
   return (
     <div className="bg-myPrimary -skew-x-12 rounded-xl inline-flex justify-self-start">
-      <p className="p-0 md:p-4 skew-x-12 text-myWhite mx-2">{text}</p>
+      <p className="p-4 md:p-4 skew-x-12 text-myWhite mx-4">{text}</p>
     </div>
   )
 }
