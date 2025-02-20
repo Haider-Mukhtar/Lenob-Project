aa1b072ec9b90405872949dd4ba94053a7997792
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-02-20 08:33:24 +0500

    Remove Star

--- src/components/success-stories-section.jsx
+++ src/components/success-stories-section.jsx
@@ -9,8 +9,8 @@ const SuccessStoriesSection = () => {
     <div className="mt-10">
       <MaxWidthWrapper className={"border-amber-300"}>
         <div className="w-full lg:w-[90%] justify-self-center border border-red-600 px-2 py-10 ">
-          <div className="pt-10 font-myFont w-full md:w-[70%] justify-self-center border border-amber-400">
-            <img className="aspect-square justify-self-start" src={SmallBlueStar} alt="Small-White-Star" />
+          <div className="font-myFont w-full md:w-[70%] justify-self-center border border-amber-400">
+            {/*<img className="aspect-square justify-self-start" src={SmallBlueStar} alt="Small-White-Star" />*/}
             <div className="text-2xl md:text-5xl text-myBlack text-center font-semibold ">
               <HighlightedText text={"Success"} />
               &nbsp;Stories of our loyal clients
